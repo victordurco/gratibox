71b5e366c280dde2306308e8ba513cae896067cd
victordurco <victordurco@gmail.com>
2021-11-19 15:58:04 -0300

    sign in page done

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,25 +1,109 @@
-import React from 'react';
+/* eslint-disable consistent-return */
+import React, { useState, useContext, useEffect } from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
 import Button from '../shared/Button';
 import WelcomeTitle from '../shared/WelcomeTitle';
 import AuthInput from '../shared/AuthInput';
+import { signIn } from '../../services/gratibox.services';
+import UserContext from '../../contexts/UserContext';
 
-const SignIn = () => (
-  <Container>
-    <WelcomeTitle />
-    <AuthInput placeholder="Email" type="email" />
-    <AuthInput placeholder="Senha" type="password" />
-    <Button
-      width="60vw"
-      height="56px"
-      text="Login"
-      type="submit"
-      marginTop="35px"
-      marginBottom="15px"
-    />
-    <SignUpButton>Ainda não sou grato</SignUpButton>
-  </Container>
-);
+const SignIn = () => {
+  const navigate = useNavigate();
+  const { setUser, user } = useContext(UserContext);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleChange = (prop) => (event) => {
+    setFormData({ ...formData, [prop]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (formData.password.length < 6) {
+      return (
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Sua senha deve conter pelo menos 6 caracteres',
+        })
+      );
+    }
+
+    signIn(formData)
+      .then((response) => {
+        localStorage.setItem('token', JSON.stringify(response.data.token));
+        setUser({ ...response.data });
+        navigate('/');
+      }).catch((error) => {
+        const { status } = error.response;
+        if (status === 404) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'E-mail não cadastrado',
+          });
+        } else if (status === 401) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Senha incorreta',
+          });
+        } else if (status === 400) {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Dados preenchidos incorretamente',
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Falha ao entrar, tente novamente mais tarde',
+          });
+        }
+      });
+  };
+
+  useEffect(() => {
+    if (user) {
+      navigate('/');
+    }
+  }, []);
+
+  return (
+    <Container>
+      <WelcomeTitle />
+      <Form onSubmit={handleSubmit}>
+        <AuthInput
+          placeholder="Email"
+          type="email"
+          value={formData.email}
+          onChange={handleChange('email')}
+        />
+        <AuthInput
+          placeholder="Senha"
+          type="password"
+          value={formData.password}
+          onChange={handleChange('password')}
+        />
+        <Button
+          width="60vw"
+          height="56px"
+          text="Login"
+          type="submit"
+          marginTop="35px"
+          marginBottom="15px"
+        />
+        <SignUpButton onClick={() => navigate('/cadastro')}>Ainda não sou grato</SignUpButton>
+      </Form>
+    </Container>
+  );
+};
 
 export default SignIn;
 
@@ -43,6 +127,14 @@ const SignUpButton = styled.button`
   border-radius:10px;
   background-color: inherit;
   display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
